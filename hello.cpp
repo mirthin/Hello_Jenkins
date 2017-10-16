80390ee87cc66e06294293479de4cd923b90090e
mirthin <mirthin@users.noreply.github.com>
2017-10-16 12:38:33 +0200

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -21,8 +21,6 @@ int main(int argc, char **argv)
   cout << "Hello, Jenkins\n";
 
   cout << "I have successfully built and run\n";
-  ::testing::InitGoogleTest(&argc, argv);
+  //::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
-  
-  return 0;
 }
