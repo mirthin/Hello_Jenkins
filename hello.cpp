495b15031d6d76cb05a314cb0bdc9675b342ba24
mirthin <mirthin@users.noreply.github.com>
2017-10-16 12:40:37 +0200

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -7,13 +7,13 @@ using namespace std;
 double m_pow(double in) {
   return in*in;
 }
-/*
+
 TEST (SquareRootTest, PositiveNos) { 
     EXPECT_EQ (4.0, m_pow (2));
     EXPECT_EQ (9.0, m_pow (3));
     EXPECT_EQ (4.0, m_pow (-2));
 }
-*/
+
 
 int main(int argc, char **argv)
 {
@@ -21,6 +21,6 @@ int main(int argc, char **argv)
   cout << "Hello, Jenkins\n";
 
   cout << "I have successfully built and run\n";
-  //::testing::InitGoogleTest(&argc, argv);
+  ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
